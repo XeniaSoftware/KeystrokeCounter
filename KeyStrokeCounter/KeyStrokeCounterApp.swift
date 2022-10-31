f92dd0c191396f4d2f0803768220004a8179d667
William Staggs <William@staggs.io>
2022-10-30 22:00:20 -0500

    Initial Commit

---
+++ KeyStrokeCounter/KeyStrokeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyStrokeCounterApp.swift
+//  KeyStrokeCounter
+//
+//  Created by William Staggs on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct KeyStrokeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
