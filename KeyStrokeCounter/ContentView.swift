44248a27c29f707337627da63831a0ee54fe8ef9
William Staggs <William@staggs.io>
2022-10-31 09:11:56 -0500

    Initial Commit

--- KeyStrokeCounter/ContentView.swift
+++ KeyStrokeCounter/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var error: String? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if error != nil {
+                Text(error!)
+            }
+            Text(String(self.count))
+            HStack {
+                Button("Reset") {
+                    count = 0
+                }
+            }
         }
         .padding()
+        .onAppear(perform: { () in
+            let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String : true]
+            let accessEnabled = AXIsProcessTrustedWithOptions(options)
+
+            if !accessEnabled {
+                error = "Access Not Enabled"
+            }
+            NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { (event) in
+                guard let _ = event.characters else {
+                    return
+                }
+                self.count += 1
+            }
+        })
     }
 }
 
