6453b82d0bb3cd9940f302ce7886ad6d72be6f97
William Staggs <William@staggs.io>
2022-10-31 22:57:39 -0500

    Add persistence

--- KeyStrokeCounter/Model/KeyStrokeCollectionModel.swift
+++ KeyStrokeCounter/Model/KeyStrokeCollectionModel.swift
@@ -11,6 +11,12 @@ import AppKit
 class KeyStrokeCollectionModel: ObservableObject {
     @Published var keystrokeCollection: Dictionary<String, KeyStrokeModel> = [:]
     
+    init(_ shouldLoad: Bool = true) {
+        if shouldLoad {
+            load()
+        }
+    }
+    
     var total: Int {
         print("Getting Total")
         return getModel().total
@@ -44,5 +50,51 @@ class KeyStrokeCollectionModel: ObservableObject {
         let model = getModel()
         model.handle(event: event)
         objectWillChange.send()
+        save()
+    }
+}
+
+extension KeyStrokeCollectionModel {
+    // MARK: Persistence
+    private static var documentsFolder: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask,
+                                               appropriateFor: nil,
+                                               create: false)
+        } catch {
+            fatalError("Can't find documents directory.")
+        }
+    }
+    
+    private static var fileURL: URL {
+        return documentsFolder.appendingPathComponent("BGGCollection.data")
+    }
+    
+    func load() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.fileURL) else {
+                return
+            }
+            guard let collections = try? JSONDecoder().decode(Dictionary<String, KeyStrokeModel>.self, from: data) else {
+                fatalError("Can't decode saved contact data.")
+            }
+            DispatchQueue.main.async {
+                self?.keystrokeCollection = collections
+            }
+        }
+    }
+    
+    func save() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let collections = self?.keystrokeCollection else { fatalError("Self out of scope") }
+            guard let data = try? JSONEncoder().encode(collections) else { fatalError("Error encoding data") }
+            do {
+                let outfile = Self.fileURL
+                try data.write(to: outfile)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
     }
 }
