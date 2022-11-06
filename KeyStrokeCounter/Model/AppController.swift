fac0a04027706fd59b1cfaca22831e3243c21b1a
William Staggs <William@staggs.io>
2022-11-06 11:32:17 -0600

    Check if data is 0 bytes

--- KeyStrokeCounter/Model/AppController.swift
+++ KeyStrokeCounter/Model/AppController.swift
@@ -67,6 +67,9 @@ extension AppController {
         DispatchQueue.global(qos: .background).async { [weak self] in
             guard let appModel = self?.appModel else { fatalError("Self out of scope") }
             guard let data = try? JSONEncoder().encode(appModel) else { fatalError("Error encoding data") }
+            if data.count == 0 {
+                fatalError("ATTEMPTING TO WRITE 0 bytes")
+            }
             do {
                 let outfile = Self.fileURL
                 try data.write(to: outfile)
