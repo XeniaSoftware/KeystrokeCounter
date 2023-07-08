18b8df632e578ec90440ab6b4933ba860588977c
William Staggs <william@staggs.io>
2023-07-08 17:57:27 -0500

    Fix settings window for ventura

--- KeystrokeCounter/Model/AppController.swift
+++ KeystrokeCounter/Model/AppController.swift
@@ -109,7 +109,11 @@ class AppController: ObservableObject {
         if let window = windows[.Settings] {
             window.focus()
         } else {
-            NSApp.sendAction(Selector(("showPreferencesWindow:")), to: nil, from: nil)
+            if #available(macOS 13, *) {
+              NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
+            } else {
+              NSApp.sendAction(Selector(("showPreferencesWindow:")), to: nil, from: nil)
+            }
         }
     }
 }
